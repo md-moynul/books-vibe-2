42121e301e6d186909034b0d2259c461ba9ddf78
md-moynul <mdmoynulislam58995@gmail.com>
2026-04-17 11:04:18 +0600

    un comment pradesto read

--- src/pages/Pages to read/PagesToRead.jsx
+++ src/pages/Pages to read/PagesToRead.jsx
@@ -1,30 +1,11 @@
-// import React from 'react';
+import React from 'react';
 
-// const PagesToRead = () => {
-//     return (
-//         <div>
-//                 <BarChart
-//       style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
-//       responsive
-//       data={data}
-//       margin={{
-//         top: 20,
-//         right: 0,
-//         left: 0,
-//         bottom: 5,
-//       }}
-//     >
-//       <CartesianGrid strokeDasharray="3 3" />
-//       <Tooltip cursor={{ fillOpacity: 0.5 }} />
-//       <XAxis dataKey="name" />
-//       <YAxis width="auto" />
-//       <Bar dataKey="uv" fill="#8884d8" shape={TriangleBar} activeBar>
-//         <LabelList content={CustomColorLabel} position="top" />
-//       </Bar>
-//       <RechartsDevtools />
-//     </BarChart>
-//         </div>
-//     );
-// };
+const PagesToRead = () => {
+    return (
+        <div>
+               
+        </div>
+    );
+};
 
-// export default PagesToRead;
+export default PagesToRead;
